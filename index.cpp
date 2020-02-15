d305483cec503a10b792b212d1784110707e7dcf
confrednabukenya <confrednabukenya@gmail.com>
2020-02-15 17:17:17 +0300

    Aprogram to display CONFRED 5 times using forloop

---
+++ index.cpp
@@ -0,0 +1,13 @@
+// A program that displays CONFRED five times
+#include<iostream>
+using namespace std;
+int main(){
+int i;
+for(i=1;i<=5;i++){
+	
+	cout<<"CONFRED"<<endl;
+}	
+	
+	
+	return 0;
+}
